da9360e7224369ba1e1925859d4d923cbc882bc5
sam@ukjp <sam.aldis@gmail.com>
2023-05-05 02:50:19 +0100

    Update main.py
    less concise version added

--- src/main.py
+++ src/main.py
@@ -6,3 +6,19 @@ def fizzbuzz(i):
 
 for i in range(0,102):
     fizzbuzz(i)
+
+#less concise
+
+def fizzbuzz(i):
+		n = [i%3, i%5]
+		if n[0]==0:
+				if n[1] == 0:
+						print("fizzbuzz")
+				else:
+						print("fizz")
+		elif n[1] == 0:
+			print("buzz")
+
+if __name__ == "__main__":
+	for x in range(0,102):
+		fizzbuzz2(x)
